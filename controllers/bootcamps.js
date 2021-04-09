7cc920ee7acb88faeaf7e6122c10e547936b3082
RobertoRamos <robram91@gmail.com>
2021-04-08 23:40:55 -0400

    testing get methods

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -3,17 +3,32 @@ const Bootcamp = require('../models/Bootcamp')
 // @desc    Get all bootcamps
 // @route   GET /api/v1/bootcamps
 // @access Public
-exports.getBootcamps = (req, res, next) => {
-  res.status(200).json({ success: true, msg: 'Show all bootcamps' });
+exports.getBootcamps = async (req, res, next) => {
+  try {
+    const bootcamps = await Bootcamp.find();
+    res.status(200).json({
+      success: true,
+      data: bootcamps
+    })
+  } catch (error) {
+    res.status(400).json({
+      success: false
+    })
+  }
 };
 
 // @desc    Get single bootcamp
 // @route   GET /api/v1/bootcamps/:id
 // @access Public
-exports.getBootcamp = (req, res, next) => {
-  res
-    .status(200)
-};
+exports.getBootcamp = async (req, res, next) => {
+  try {
+    const bootcamp = await Bootcamp.findById(req.params.id);
+
+    res.status(200).json({ success: true, data: bootcamp })
+  } catch (error) {
+    res.status(400).json({ success: false })
+  }
+}
 
 // @desc    Create new bootcamp
 // @access Private
