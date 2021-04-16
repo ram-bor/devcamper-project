89c62bd8e8978ec6f914190c88b5c9c218bdebf9
RobertoRamos <robram91@gmail.com>
2021-04-16 00:01:05 -0400

    array of obj

--- middleware/error.js
+++ middleware/error.js
@@ -22,8 +22,8 @@ const errorHandler = (err, req, res, next) => {
 
     // Mongoose Validation error
     if (err.name === 'ValidationError') {
-        const message = Object.values(err.errors)
-
+        const message = Object.values(err.errors).map(val => val.message)
+        error = new ErrorResponse(message, 400)
     }
     res.status(error.statusCode || 500).json({
         success: false,
