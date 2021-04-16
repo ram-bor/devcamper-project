11f4f6a20b72beef1ee5f61fcbddd4504b855ad6
RobertoRamos <robram91@gmail.com>
2021-04-15 23:52:33 -0400

    err code + name

--- middleware/error.js
+++ middleware/error.js
@@ -13,7 +13,11 @@ const errorHandler = (err, req, res, next) => {
         const message = `Resource not found with id of ${err.value}`
         error = new ErrorResponse(message, 404);
     }
-    console.log(err.name)
+
+    if (err.code === 11000) {
+        const message = 'Duplicate field value entered'
+        error = new ErrorResponse(message, 400)
+    }
     res.status(error.statusCode || 500).json({
         success: false,
         error: error.message || 'Server Error'
