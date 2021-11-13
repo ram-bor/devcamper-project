8053f08601d932c6f065a28601e832e576ba13f5
RobertoRamos <robram91@gmail.com>
2021-11-12 19:32:34 -0500

    running

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const morgan = require('morgan')
 const colors = require('colors')
 const connectDB = require('./config/db')
 const errorHandler = require('./middleware/error')
+
 // Load env vars
 dotenv.config({ path: './config/config.env' });
 
